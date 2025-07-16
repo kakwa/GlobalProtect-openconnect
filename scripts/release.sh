54b77888acb6615f83b132c25d28efb78fb40ac5
carpenti <carpenti@adobe.com>
2025-07-17 00:07:33 +0200

    clean-up

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,6 @@
 
 VERSION=$(cat VERSION)
 
-# Update packaging, e.g., version, changelog, etc.
-./scripts/prepare-packaging.sh
-
 # Commit the changes
 git commit -m "Release ${VERSION}" .
 git tag v$VERSION -a -m "Release ${VERSION}"
